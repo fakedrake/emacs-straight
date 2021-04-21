de13b23a17a3a8374814d9d2abff404f84685946
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-04-21 12:52:19 +0100

    Grep stuff

--- lisp/fd-grep.el
+++ lisp/fd-grep.el
@@ -1,7 +1,13 @@
+(defun tags-find-symbol (symbol file-domain)
+  (interactive (list
+                (rg-read-pattern 'literal (grep-tag-default)) (rg-read-files)))
+  (rg-project (format "\\b%s\\b" (regexp-quote symbol)) file-domain))
+
 (use-package wgrep)
 (use-package rg
   :after (wgrep)
   :config
   (defalias 'rgrep 'rg)
+  (setq rg-executable (executable-find "rg"))
   (autoload 'wgrep-rg-setup "wgrep-rg")
   (add-hook 'rg-mode-hook 'wgrep-rg-setup))
--- lisp/fd-rust.el
+++ lisp/fd-rust.el
@@ -1,17 +1,18 @@
 (add-to-list 'exec-path "/Users/cperivol/.cargo/bin")
 
 (defun fd-rust-mode-after-save ()
+  (rust-update-buffer-project)
   (let ((default-directory
           (or (and rust-buffer-project
                    (file-name-directory rust-buffer-project))
               default-directory)))
-    (start-process "rusty-tags-process" "*rusty-tags-process*" "rusty-tags" "emacs")))
+    (start-process "rusty-tags-process" "*rusty-tags-process*" "rusty-tags" "emacs" "--output=TAGS")))
 
 (defun fd-rust-mode-hook ()
   (add-hook 'after-save-hook 'fd-rust-mode-after-save nil t))
 
 (use-package rust-mode
-  :hooks ((rust-mode . fd-rust-mode-hook))
+  :hook ((rust-mode . fd-rust-mode-hook))
   :config
   (require 'fd-gud-lldb))
 
